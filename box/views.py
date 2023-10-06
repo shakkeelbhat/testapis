8dffa13ff14f50efaa183926036c2a404d23b7e4
shakkeelbhat <shakkeelbhat@gmail.com>
2023-10-06 11:22:53 +0530

    registerApi

--- box/views.py
+++ box/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializers  import registerSerializer, loginSerializer, boxSerializer
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
 
 
 class registerApi(APIView):
-    pass
+    def post(self,request):
+
+        try:
+            data  = request.data
+            serializer  = registerSerializer(data=data)
+            if not serializer.is_valid():
+                return Response({'message':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({'message':'User registered'},status=status.HTTP_200_OK)
+        except:
+            return Response({'message':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+
 
 class loginApi(APIView):
     pass
