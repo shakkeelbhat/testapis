172d424695222ee9e37ac28c6f97106afc83daad
shakkeelbhat <shakkeelbhat@gmail.com>
2023-10-06 11:31:00 +0530

    login added

--- box/views.py
+++ box/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .serializers  import registerSerializer, loginSerializer, boxSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 # Create your views here.
 
 
@@ -22,8 +23,19 @@ class registerApi(APIView):
 
 
 class loginApi(APIView):
-    pass
-
+    def post(self,request):
+        try:
+            data  = request.data
+            serializer  = loginSerializer(data=data)
+            if not serializer.is_valid():
+                return Response({'message':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+            user  = authenticate(username=serializer.data["username"],password=serializer.data["password"])
+            if not user:
+                return Response({'message':'User doesnt exist'},status=status.HTTP_400_BAD_REQUEST)
+            
+            return Response({'message':'User logged in'},status=status.HTTP_200_OK)
+        except:
+            return Response({'message':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
 class listApi(APIView):
     pass
 
