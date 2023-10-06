1cca681ab485d99b6203e67fd8fa7592b394a174
shakkeelbhat <shakkeelbhat@gmail.com>
2023-10-06 11:15:32 +0530

    model created

--- box/models.py
+++ box/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class BoxModel():
-    pass
+class BoxModel(models.Model):
+    name = models.CharField(max_length=50,null=False,blank=False)
+    shape = models.CharField(max_length=50,null=False,blank=False)
+    price = models.IntegerField()
+
